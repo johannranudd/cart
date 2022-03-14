7e741d1e18cef2ab378b931dbc8ec6e9128745d9
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-03-14 16:53:13 +0100

    starting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return (
-    <div className='App'>
-      <h2>app</h2>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledDiv } from './Navbar.style';
+
+const Navbar = () => {
+  return <StyledDiv>Navbar</StyledDiv>;
+};
+
+export default Navbar;
