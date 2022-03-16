e982ed997b0241f8154829dd59a0e6fcf369a068
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-03-16 14:48:34 +0100

    reducing cart to one item with same ID

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGlobalContext } from '../../context';
 import { StyledDiv } from './Cart.style';
 import Navbar from '../Navbar/Navbar';
 
 const Cart = () => {
   const { inCart } = useGlobalContext();
-  console.log(inCart);
+  const [newCart, setNewCart] = useState([]);
+
+  const redu = inCart.reduce((total, value) => {
+    if (!total.includes(value.id)) {
+      total.push(value.id);
+    }
+    return total;
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -13,14 +21,14 @@ const Cart = () => {
         <section className='section-center'>
           <h2>In Cart</h2>
           <ul>
-            {inCart.map((item, index) => {
+            {/* {newCart.map((item, index) => {
               const { id, title, price, img } = item;
               return (
-                <li key={index}>
+                <li key={id}>
                   <img src={img} alt={title} />
                 </li>
               );
-            })}
+            })} */}
           </ul>
         </section>
       </StyledDiv>
--- src/components/Cart/Cart.style.jsx
+++ src/components/Cart/Cart.style.jsx
@@ -2,4 +2,19 @@ import styled from 'styled-components';
 
 export const StyledDiv = styled.div`
   /* background-color: green; */
+  .section-center {
+    width: 100%;
+    max-width: 700px;
+    margin: auto;
+    h2 {
+      text-align: center;
+    }
+    ul {
+      li {
+        img {
+          max-width: 200px;
+        }
+      }
+    }
+  }
 `;
